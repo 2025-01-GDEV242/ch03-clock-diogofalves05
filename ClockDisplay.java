a9316df2276752f7de9030ca4bd4abbd6e19b7cd
Diogo Alves <diogofalves05@gmail.com>
2025-02-10 11:51:19 -0500

    Updated Author & Version in 12 Hour Branch

--- ClockDisplay.java
+++ ClockDisplay.java
@@ -13,8 +13,8 @@
  * and reacts by incrementing the display. This is done in the usual clock
  * fashion: the hour increments when the minutes roll over to zero.
  * 
- * @author Michael Kölling and David J. Barnes
- * @version 2016.02.29
+ * @author Diogo Alves 
+ * @version 2025.02.10
  */
 public class ClockDisplay
 {
