cc65a42c33d8de1ffd3fa77586099ab34c6c07b2
Diogo Alves <diogofalves05@gmail.com>
2025-02-10 11:44:00 -0500

    Update Clock Display For 24 Hour Internal Header Comment

--- ClockDisplay.java
+++ ClockDisplay.java
@@ -1,16 +1,20 @@
 
 /**
- * The ClockDisplay class implements a digital clock display for a
- * European-style 24 hour clock. The clock shows hours and minutes. The 
- * range of the clock is 00:00 (midnight) to 23:59 (one minute before 
- * midnight).
+ * 24 HOUR INTERNAL BRANCH 
+ * 
+ * The ClockDisplay class implements a digital clock display for a 
+ * American-style 12 hour clock. The clock shows hours and minutes. 
+ * 
+ * The internal range of the clock is 00:00 (midnight) to 23:59 
+ * (one minute before 
+ *  midnight).
  * 
  * The clock display receives "ticks" (via the timeTick method) every minute
  * and reacts by incrementing the display. This is done in the usual clock
  * fashion: the hour increments when the minutes roll over to zero.
  * 
- * @author Michael Kölling and David J. Barnes
- * @version 2016.02.29
+ * @author Diogo Alves 
+ * @version 2025.02.10 
  */
 public class ClockDisplay
 {
