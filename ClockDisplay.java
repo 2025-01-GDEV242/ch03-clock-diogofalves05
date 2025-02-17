5cb5ccd6f97ff55272be29abbccbf9649f9fcf8f
Diogo Alves <diogofalves05@gmail.com>
2025-02-17 16:27:17 -0500

    Changed The UpdateDisplay In ClockDisplay

--- ClockDisplay.java
+++ ClockDisplay.java
@@ -80,9 +80,21 @@ public class ClockDisplay
     /**
      * Update the internal string that represents the display.
      */
-    private void updateDisplay()
-    {
-        displayString = hours.getDisplayValue() + ":" + 
-                        minutes.getDisplayValue();
+    /**
+* Update the internal string that represents the display.
+*/
+private void updateDisplay()
+{
+    int hour = hours.getValue();
+    String suffix = "am";
+    if(hour >= 12) {
+        hour = hour - 12;
+        suffix = "pm";
     }
+    if(hour == 0) {
+        hour = 12;
+    }
+    displayString = hour + "." + minutes.getDisplayValue() +
+    suffix;
+}
 }
